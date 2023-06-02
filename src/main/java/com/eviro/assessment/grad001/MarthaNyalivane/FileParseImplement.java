396eeaf09b256890029e0442a0777ea42ba56801
Amanda <amanda@pop-os.localdomain>
2023-06-02 08:05:02 +0200

    updaated files

---
+++ src/main/java/com/eviro/assessment/grad001/MarthaNyalivane/FileParseImplement.java
@@ -0,0 +1,21 @@
+package com.eviro.assessment.grad001.MarthaNyalivane;
+
+import java.io.File;
+import java.net.URL;
+
+public class FileParseImplement implements FileParser{
+    @Override
+    public void parseCSV(File csvFile) {
+
+    }
+
+    @Override
+    public File convertCSVDataToImage(String base64ImageData) {
+        return null;
+    }
+
+    @Override
+    public URL createImageLink(File fileImage) {
+        return null;
+    }
+}
